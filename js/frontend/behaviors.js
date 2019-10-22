bdb60b9a38c9f06c1981e7ba21ea8347fd734352
Steven <stevenhughes08@gmail.com>
2019-10-22 19:52:27 -0400

    added more pong code

--- js/frontend/behaviors.js
+++ js/frontend/behaviors.js
@@ -46,3 +46,51 @@ document.addEventListener('keydown', function (e) {
 
 }, false);
 
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == 87 || e.which == 87) { //W key
+        speedOfPaddle1 = -10;
+    }
+
+    if (e.keyCode == 83 || e.which == 83) { // S key
+        speedOfPaddle1 = 10;
+    }
+    if (e.keyCode == 38 || e.which == 38) { // up arrow
+        speedOfPaddle2 = -10;
+    }
+
+    if (e.keyCode == 40 || e.which == 40){ //down arrow
+        speedOfPaddle2 = 10;
+
+    }
+}, false);
+
+//this function gets called 60 times per second 
+
+window.setInterval(function show()
+{
+    positionOfPaddle1 += speedOfPaddle1;
+    positionOfPaddle2 += speedOfPaddle2;
+
+    document.getElementById('paddle1').style.top = (positionOfPaddle1) + 'px';
+    document.getElementById('paddle2').style.top = (positionOfPaddle2) + 'px';
+
+}, 1000/60);
+
+
+//This section stops the paddles
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == 87 || e.which == 87) {
+        speedOfPaddle1 = 0;
+    }
+    if (e.keyCode == 83 || e.which == 83) {
+        speedOfPaddle1 = 0;
+    }
+    if (e.keyCode == 38 || e.which == 38) {
+        speedOfPaddle2 = 0;
+    }
+    if (e.keyCode == 40 || e.which == 40) {
+        speedOfPaddle2 = 0;
+    }
+}, false);
+
+
